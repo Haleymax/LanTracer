ad05f7342ce42d46577299974b4c8345179aa7f0
haley huang <huanghongweimax@163.com>
2025-01-18 17:49:48 +0800

    add table

--- server/device/models.py
+++ server/device/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Node(models.Model):
+    id = models.AutoField(primary_key=True)
+    ip_address = models.GenericIPAddressField(unique=True)
+    node_name = models.CharField(max_length=50, null=True)
+    total_menory = models.IntegerField(default=0, null=True)
+    remaining_menory = models.IntegerField(default=0, null=True)
+    state = models.CharField(max_length=50, null=True, default='offline')
+
+
+    def __str__(self):
+        return self.node_name
+
+
+
+class Device(models.Model):
+    id = models.AutoField(primary_key=True)
+    ip_address = models.GenericIPAddressField(unique=True)
+    node_name = models.CharField(max_length=50, null=True)
--- server/server/settings.py
+++ server/server/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tracer',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3308',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
