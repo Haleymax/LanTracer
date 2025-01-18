3b511173e0821ae0dbb7ce324a44900810303213
haley huang <huanghongweimax@163.com>
2025-01-18 16:49:42 +0800

    add a page

---
+++ web/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="common-layout">
+      <el-container>
+        <el-aside width="100px">
+          <Menu></Menu>
+        </el-aside>
+        <el-container>
+          <el-header>Header</el-header>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </div>
+</template>
+
+<script setup lang="ts" name="Home">
+import { ElContainer, ElAside, ElHeader, ElMain } from 'element-plus';
+import Menu from '@/components/Menu.vue';
+
+</script>
+
+<style scoped>
+.common-layout {
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
