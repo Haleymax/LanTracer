94638b271e27e6a0a90e7dd2b0a7194ec024d895
Haley <huanghongweimax@163.com>
2025-03-10 18:56:06 +0800

    add api

---
+++ web/src/api/get_device.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import type { AxiosResponse } from 'axios';
+
+interface GetDeviceResponse {
+  device: unknown;
+  status: boolean;
+}
+
+export const getDevice = async (url: string): Promise<GetDeviceResponse> => {
+  try {
+    const response: AxiosResponse = await axios.get(url);
+    return {
+      device: response.data,
+      status: true,
+    };
+  } catch (error) {
+    console.log('error :', error);
+    return {
+      device: null,
+      status: false,
+    };
+  }
+}
