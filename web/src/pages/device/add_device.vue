9b540a9b181a705167c6c3919d07114b90953791
haley huang <huanghongweimax@163.com>
2025-03-17 22:43:33 +0800

    add device page

--- web/src/pages/device/add_device.vue
+++ web/src/pages/device/add_device.vue
@@ -1,10 +1,55 @@
 <template>
-添加设备
-</template>
+    <v-sheet class="mx-auto" width="300">
+      <v-form @submit.prevent>
+        <v-text-field
+          v-model="host"
+          :rules="HostRules"
+          label="主机地址"
+        ></v-text-field>
+        <v-text-field
+          v-model="name"
+          :rules="NameRules"
+          label="主机名称"
+        ></v-text-field>
+        <v-btn class="mt-2" type="submit" block @click="submit">Submit</v-btn>
+      </v-form>
+    </v-sheet>
+  </template>
 
 <script setup lang="ts" name="AddDevice">
 import { ref } from 'vue'
-import { useDeviceStore } from '@/store/device'
+import { addDevice } from '@/api/add_device'
+const host = ref('')
+const HostRules = [
+  (v: string) => !!v || '主机地址不能为空',
+]
 
-const deviceStore = useDeviceStore()
+const name = ref('')
+const NameRules = [
+  (v: string) => !!v || '主机名称不能为空',
+]
+
+
+const submit = async () => {
+    const data = {
+      host: host.value,
+      name: name.value
+    }
+
+    const url:string = '/add_device'
+
+    try {
+      const response = await addDevice(url, data)
+      if (response.status) {
+        alert('Device added')
+      } else {
+        alert('Device not added')
+      }
+    } catch (error) {
+      alert('Error')
+    }
+}
+
+
+
 </script>
